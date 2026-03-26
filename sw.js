c66c60c19e89282682298cbd7d5ebe56b4d98f84
DJ Nurre <DJ@MyPuter.local>
2026-03-25 22:42:34 -0700

    Shorten device note text

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'map-quest-v23';
+const CACHE = 'map-quest-v24';
 const PRECACHE = [
   './', './index.html', './manifest.json', './icon.svg',
   './oreo-logo.png', './oreo-hint.png', './d3.min.js', './topojson.min.js', './states-10m.json'
