bd2e38273d57ebbf89217b27c6e7608a9957278e
DJ Nurre <DJ@MyPuter.local>
2026-03-25 22:33:28 -0700

    Remove MAP QUIZ from logo; plain section headers; state flags via flagcdn.com

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'map-quest-v21';
+const CACHE = 'map-quest-v22';
 const PRECACHE = [
   './', './index.html', './manifest.json', './icon.svg',
   './oreo-logo.png', './oreo-hint.png', './d3.min.js', './topojson.min.js', './states-10m.json'
